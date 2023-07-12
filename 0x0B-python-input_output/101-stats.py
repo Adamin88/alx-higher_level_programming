7bb2f40eac1fd0d053bcdcac2a7983273c97d6eb
Adamin88 <elhihi.mohamed@gmail.com>
2023-07-12 15:51:20 +0000

    stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,51 +1,47 @@
-#!/usr/bin/python3
-"""
-Reads from standard input and computes metrics
-"""
+import sys
 
+# Initialize variables
+total_file_size = 0
+status_counts = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+line_count = 0
 
-if __name__ == "__main__":
-    import sys
+try:
+    # Read input line by line from stdin
+    for line in sys.stdin:
+        line = line.strip()
+        
+        # Parse the line
+        parts = line.split(' ')
+        file_size = int(parts[-1])
+        status_code = parts[-2]
+        
+        # Update metrics
+        total_file_size += file_size
+        if status_code in status_counts:
+            status_counts[status_code] += 1
+        
+        line_count += 1
+        
+        # Print statistics every 10 lines
+        if line_count % 10 == 0:
+            print(f"Total file size: {total_file_size}")
+            
+            # Print number of lines by status code in ascending order
+            for status_code in sorted(status_counts.keys()):
+                count = status_counts[status_code]
+                if count > 0:
+                    print(f"{status_code}: {count}")
+        
+except KeyboardInterrupt:
+    # Handle keyboard interruption (CTRL + C)
+    print("\nProgram interrupted.")
 
-    stdin = sys.stdin
+# Print final statistics
+print(f"\nTotal file size: {total_file_size}")
 
-    c = 0
-    size = 0
-    vd = ['200', '301', '400', '401', '403', '404', '405', '500']
-    st = {}
+# Print number of lines by status code in ascending order
+for status_code in sorted(status_counts.keys()):
+    count = status_counts[status_code]
+    if count > 0:
+        print(f"{status_code}: {count}")
 
-    try:
-        for line in stdin:
-            if c == 10:
-                print("File size: {}".format(size))
-                for i in sorted(st):
-                    print("{}: {}".format(i, st[i]))
-                c = 1
-            else:
-                c = c + 1
-
-            line = line.split()
-
-            try:
-                size = size + int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in vd:
-                    if st.get(line[-2], -1) == -1:
-                        st[line[-2]] = 1
-                    else:
-                        st[line[-2]] = st[line[-2]] + 1
-            except IndexError:
-                pass
-
-        print("File size: {}".format(size))
-        for i in sorted(st):
-            print("{}: {}".format(i, st[i]))
-
-    except KeyboardInterrupt:
-        print("File size: {}".format(size))
-        for i in sorted(st):
-            print("{}: {}".format(i, st[i]))
-        raise
