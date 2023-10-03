bace987c44468524769f6a1ede9b0ce436c7f22d
Adamin88 <elhihi.mohamed@gmail.com>
2023-10-03 03:14:42 +0000

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,68 +1,86 @@
 #!/usr/bin/python3
 
-"""
-This is a module for N queens.
-"""
 
-if __name__ == '__main__':
+class QueenChessBoard:
+    def __init__(self, size):
+        self.size = size
+        self.columns = []
 
+    def place_in_next_row(self, column):
+        self.columns.append(column)
+
+    def remove_in_current_row(self):
+        return self.columns.pop()
+
+    def is_this_column_safe_in_next_row(self, column):
+        row = len(self.columns)
+        for queen_column in self.columns:
+            if column == queen_column:
+                return False
+
+        for queen_row, queen_column in enumerate(self.columns):
+            if queen_column - queen_row == column - row:
+                return False
+
+        for queen_row, queen_column in enumerate(self.columns):
+            if ((self.size - queen_column) - queen_row ==
+               (self.size - column) - row):
+                return False
+
+        return True
+
+    def display(self):
+        lista = []
+        for row in range(self.size):
+            for column in range(self.size):
+                if column == self.columns[row]:
+                    lista.append([row, column])
+        print(lista, end='')
+
+
+def solve_queen(size):
+    board = QueenChessBoard(size)
+    number_of_solutions = 0
+    row = 0
+    column = 0
+
+    while True:
+        while column < size:
+            if board.is_this_column_safe_in_next_row(column):
+                board.place_in_next_row(column)
+                row += 1
+                column = 0
+                break
+            else:
+                column += 1
+
+        if (column == size or row == size):
+            if row == size:
+                board.display()
+                print()
+                number_of_solutions += 1
+                board.remove_in_current_row()
+                row -= 1
+
+            try:
+                prev_column = board.remove_in_current_row()
+            except IndexError:
+                break
+
+            row -= 1
+            column = 1 + prev_column
+
+if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    try:
-        size = int(sys.argv[1])
-    except BaseException:
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
-    if size < 4:
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    def startSolve():
-        b = [[0 for j in range(size)] for i in range(size)]
-        checkRecursive(b, 0)
-        return
-
-    def checkRecursive(b, c):
-        if (c == size):
-            solution(b)
-            return True
-        ret = False
-        for i in range(size):
-            if (checkPosition(b, i, c)):
-                b[i][c] = 1
-                ret = checkRecursive(b, c + 1) or ret
-                b[i][c] = 0
-        return ret
-
-    def checkPosition(b, r, c):
-        for i in range(c):
-            if (b[r][i]):
-                return False
-        i = r
-        j = c
-        while i >= 0 and j >= 0:
-            if(b[i][j]):
-                return False
-            i = i - 1
-            j = j - 1
-        i = r
-        j = c
-        while j >= 0 and i < size:
-            if(b[i][j]):
-                return False
-            i = i + 1
-            j = j - 1
-        return True
-
-    def solution(b):
-        solve = []
-        for i in range(size):
-            for j in range(size):
-                if(b[i][j] is 1):
-                    solve.append([i, j])
-        print(solve)
-        solve.clear()
-    startSolve()
+    solve_queen(int(sys.argv[1]))
