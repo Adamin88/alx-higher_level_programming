6820c01ec4f8a2f016ea3c5fb453551c5ecbedd2
Adamin88 <elhihi.mohamed2gmail.com>
2024-03-01 02:20:22 +0000

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,36 +1,23 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers.
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
 
-    Args:
-    - list_of_integers: A list of unsorted integers
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
 
-    Returns:
-    - peak: A peak element in the list
-    """
-    # Helper function to check if an element is a peak
-    def is_peak(arr, index):
-        if index == 0:
-            return arr[index] >= arr[index + 1]
-        elif index == len(arr) - 1:
-            return arr[index] >= arr[index - 1]
-        else:
-            return arr[index] >= arr[index - 1] and arr[index] >= arr[index + 1]
-
-    # Binary search for peak
-    left, right = 0, len(list_of_integers) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if is_peak(list_of_integers, mid):
-            return list_of_integers[mid]
-        elif list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    # Return the remaining element (if the list length is 1)
-    return list_of_integers[left]
-
-# Example usage:
-# list_of_integers = [1, 3, 5, 7, 10, 6, 4]
-# print(find_peak(list_of_integers))
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
